13fded82efc8ae1ffee722382df3dd5f6016693d
nadlabak <pavel@doshaska.net>
2012-06-20 13:42:54 +0200

    update mapphone.defconfig for pvr 1.8, disable cmdline from ATAG

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -638,7 +638,7 @@ __tagtable(ATAG_REVISION, parse_tag_revision);
 
 static int __init parse_tag_cmdline(const struct tag *tag)
 {
-	strlcpy(default_command_line, tag->u.cmdline.cmdline, COMMAND_LINE_SIZE);
+	//strlcpy(default_command_line, tag->u.cmdline.cmdline, COMMAND_LINE_SIZE);
 	return 0;
 }
 
